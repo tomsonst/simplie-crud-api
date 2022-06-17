45376785fac9f676a88ccf66f34d74bff42c8c76
MINSK\Stanislau_Tamashou <Stanislau_Tamashou@epam.com>
2022-06-17 18:20:42 +0300

    fit: added cluster

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import cluster from 'cluster';
+import os from 'os';
+import dotenv from 'dotenv';
 import {
   getAllUsersController,
   getUserByIdController,
@@ -6,11 +9,16 @@ import {
   updateUserController,
   deleteUserController
 } from './controllers/usersControllers';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
-createServer((req: IncomingMessage, res: ServerResponse) => {
+const isMultiServer = process.argv.some((item) => item === '--multi');
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  if (cluster.isWorker)
+    console.log(
+      `Worker ${cluster.worker?.id} handle request`
+    );
   if(req.url === '/api/users' && req.method === 'GET') {
     
     getAllUsersController(req, res);
@@ -31,4 +39,28 @@ createServer((req: IncomingMessage, res: ServerResponse) => {
     res.writeHead(404, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({message: 'Resource do not found'}));
   }
-}).listen(process.env.PORT); 
+});
+
+if(!isMultiServer){
+  server.listen(process.env.PORT);
+} else {
+  if (cluster.isPrimary) {
+    let cpus = os.cpus().length;
+  
+    for (let i = 0; i < cpus; i++) {cluster.fork();}
+  
+    cluster.on('exit', (worker, code) => {
+      console.log(
+        `Worker ${worker.id} finished. Exit code: ${code}`
+      );
+  
+      server.listen(process.env.PORT, () => {
+        console.log(`Worker ${cluster.worker?.id} launched`)
+      });
+    });
+  } else
+  server.listen(process.env.PORT, () => {
+    console.log(`Worker ${cluster.worker?.id} launched`)
+  });
+}
+//server.listen(process.env.PORT); 
